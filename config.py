1db46514cc74d59b29016855921f5561f175b6f1
WXFIC <48744307+WXFIC@users.noreply.github.com>
2019-07-19 21:07:04 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from __future__ import print_function
+import os
+import platform
+
+
+class Config(object):
+    """docstring for Config"""
+
+    def __init__(self):
+        super(Config, self).__init__()
+
+        # DATAPATH = os.environ.get('/home/wang/Documents/adsldata/STResNet/BikeNYC')
+        DATAPATH = '/home/wang/Documents/adsldata/STResNet'
+
+        if DATAPATH is None:
+            if platform.system() == "Windows" or platform.system() == "Linux":
+                # DATAPATH = "D:/data/traffic_flow"
+            # elif platform.system() == "Linux":
+                DATAPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data')
+            else:
+                print("Unsupported/Unknown OS: ", platform.system, "please set DATAPATH")
+        self.DATAPATH = DATAPATH
+
---
+++ iLayer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[22]:
+
+
+from tensorflow.keras import backend as K
+from tensorflow.python.layers.base import Layer
+# from keras.layers import Dense
+import numpy as np
+
+
+class iLayer(Layer):
+    def __init__(self, **kwargs):
+        # self.output_dim = output_dim
+        super(iLayer, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        initial_weight_value = np.random.random(input_shape[1:])
+        self.W = K.variable(initial_weight_value)
+        self._trainable_weights = [self.W]
+
+    def call(self, x, mask=None):
+        return x * self.W
+
+    def get_output_shape_for(self, input_shape):
+        return input_shape
+
+
+# In[ ]:
+
+
+
+
